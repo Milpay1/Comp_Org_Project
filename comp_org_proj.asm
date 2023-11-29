7711b2875332c1abba2de3e9a1bba0a304cd84be
Miles Murray <41023365+Milpay1@users.noreply.github.com>
2023-11-28 22:17:56 -0500

    Line 6 denotes that this is the main text for the program, line 7 denotes that this is the main code for the program, line 9 loads the print string value into v0, line 10 loads the address of the prompt into a0, and line 11 does the system call

--- comp_org_proj.asm
+++ comp_org_proj.asm
@@ -2,3 +2,10 @@
 prompt:     .asciiz "Enter a number (your number must be greater than or equal to 25): "
 error_msg:  .asciiz "Illegal Number!\n"
 fib_msg:    .asciiz "Fibonacci Sequence:\n"
+
+    .text
+main:
+    #Ask for number
+    li $v0, 4
+    la $a0, prompt
+    syscall
